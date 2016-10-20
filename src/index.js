ee13f2e7721fadeeddbfd9bd8cfb1893ba2afa87
Cradanimal <cradwimb@gmail.com>
2016-10-19 22:06:27 -0700

    add bombs and boardering bombs dynamically

--- src/index.js
+++ src/index.js
@@ -34,16 +34,86 @@ minesweeper.controller('mainController', function( $scope ) {
     for (let i = 0; i < size; i++) {
       $scope.grid.push([]);
       for (let j = 0; j < size; j++) {
-        $scope.grid[i].push({val : j, active : false});
+        $scope.grid[i].push({val : j, active : false, bomb : false, touchingBombs : 0, nearCells : []});
+        let nearCells = $scope.grid[i][j].nearCells
+        if (j !== 0) {
+          nearCells.push([i, j - 1]);
+          if (i !== 0) {
+            nearCells.push([i - 1, j])
+            nearCells.push([i - 1, j - 1]);
+          }
+          if (i !== size - 1) {
+            nearCells.push([i + 1, j])
+            nearCells.push([i + 1, j - 1]);
+          }
+        }
+        if (j !== size - 1) {
+          nearCells.push([i, j + 1]);
+          if (i !== 0) {
+            nearCells.push([i - 1, j + 1]);
+          }
+          if (i !== size - 1) {
+            nearCells.push([i + 1, j + 1]);
+          }
+        }
       }
     }
+    console.log('so far')
 
+    addAndTrackBombs(size, bombCount);
+
+
+    $scope.grid.forEach(function(row) {
+      row.forEach(function(cell) {
+        console.log(cell);
+      });
+    });
+  };
+
+  function addAndTrackBombs (size, bombCount) {
     for (let i = 0; i < bombCount; i++) {
       let bomb = getRandomIndex(size);
       console.log(bomb);
-      $scope.grid[bomb[0]][bomb[1]].active = true;
+      if (!$scope.grid[bomb[0]][bomb[1]].bomb) {
+        $scope.grid[bomb[0]][bomb[1]].nearCells.forEach(function(tuple) {
+          $scope.grid[tuple[0]][tuple[1]].touchingBombs++;
+        });
+      }
+      $scope.grid[bomb[0]][bomb[1]].bomb = true;
     }
-  }
+  };
+
+  function near (cell) {
+      let nearCells = [];
+      let cellIndex = cell.$index;
+      let currentRow = cell.$parent;
+      console.log(cell.$parent);
+      let nextRow = $scope.grid[cell.$parent.$index + 1];
+      let prevRow = $scope.grid[cell.$parent.$index - 1];
+      let leftCheck = cellIndex > 0;
+      let rightCheck = cellIndex < currentRow.length - 1;
+
+      if (rightCheck) {
+        nearCells.push(currentRow[cellIndex + 1]);
+        if (prevRow) {
+          nearCells.push(prevRow[cellIndex + 1])
+        }
+        if (nextRow) {
+          nearCells.push(nextRow[cellIndex + 1])
+        }
+      }
+      if (leftCheck) {
+        nearCells.push(currentRow[cellIndex - 1]);
+        if (prevRow) {
+          nearCells.push(prevRow[cellIndex - 1])
+        }
+        if (nextRow) {
+          nearCells.push(nextRow[cellIndex - 1])
+        }
+      }
+      console.log(nearCells);
+      return nearCells;
+    };
 
 
 
