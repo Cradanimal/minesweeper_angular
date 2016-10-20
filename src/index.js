2d7e65b9ae29c88c01cd32db550a493187bb246a
Cradanimal <cradwimb@gmail.com>
2016-10-20 13:13:09 -0700

    update repo

--- src/index.js
+++ src/index.js
@@ -23,6 +23,24 @@ minesweeper.controller('mainController', function( $scope ) {
 
   }
 
+  $scope.revealCell = function(cell, x, y) {
+    if (cell) {
+      x = cell.$parent.$index;
+      y = cell.$index;
+    }
+    if ($scope.grid[x][y].active === false) {
+      $scope.grid[x][y].active = true;
+    }
+    
+    if ($scope.grid[x][y].touchingBombs === '') {
+      $scope.grid[x][y].nearCells.forEach(function(tuple) {
+        if ($scope.grid[tuple[0]][tuple[1]].active === false){
+          $scope.revealCell(null, tuple[0], tuple[1]);
+        }
+      });
+    }
+  }
+
 
   function getRandomIndex (size) {
     return [Math.floor(Math.random() * size),Math.floor(Math.random() * size)]
@@ -34,7 +52,7 @@ minesweeper.controller('mainController', function( $scope ) {
     for (let i = 0; i < size; i++) {
       $scope.grid.push([]);
       for (let j = 0; j < size; j++) {
-        $scope.grid[i].push({val : j, active : false, bomb : false, touchingBombs : 0, nearCells : []});
+        $scope.grid[i].push({val : j, active : false, bomb : false, touchingBombs : '', nearCells : []});
         let nearCells = $scope.grid[i][j].nearCells
         if (j !== 0) {
           nearCells.push([i, j - 1]);
@@ -58,17 +76,11 @@ minesweeper.controller('mainController', function( $scope ) {
         }
       }
     }
-    console.log('so far')
 
     addAndTrackBombs(size, bombCount);
 
-
-    $scope.grid.forEach(function(row) {
-      row.forEach(function(cell) {
-        console.log(cell);
-      });
-    });
   };
+
 
   function addAndTrackBombs (size, bombCount) {
     for (let i = 0; i < bombCount; i++) {
@@ -76,47 +88,49 @@ minesweeper.controller('mainController', function( $scope ) {
       console.log(bomb);
       if (!$scope.grid[bomb[0]][bomb[1]].bomb) {
         $scope.grid[bomb[0]][bomb[1]].nearCells.forEach(function(tuple) {
-          $scope.grid[tuple[0]][tuple[1]].touchingBombs++;
+          if ($scope.grid[tuple[0]][tuple[1]].touchingBombs === '') {
+            $scope.grid[tuple[0]][tuple[1]].touchingBombs = 1;
+          } else {
+            $scope.grid[tuple[0]][tuple[1]].touchingBombs++;
+          }
         });
       }
       $scope.grid[bomb[0]][bomb[1]].bomb = true;
     }
   };
 
+
   function near (cell) {
-      let nearCells = [];
-      let cellIndex = cell.$index;
-      let currentRow = cell.$parent;
-      console.log(cell.$parent);
-      let nextRow = $scope.grid[cell.$parent.$index + 1];
-      let prevRow = $scope.grid[cell.$parent.$index - 1];
-      let leftCheck = cellIndex > 0;
-      let rightCheck = cellIndex < currentRow.length - 1;
+    let nearCells = [];
+    let cellIndex = cell.$index;
+    let currentRow = cell.$parent;
+    console.log(cell.$parent);
+    let nextRow = $scope.grid[cell.$parent.$index + 1];
+    let prevRow = $scope.grid[cell.$parent.$index - 1];
+    let leftCheck = cellIndex > 0;
+    let rightCheck = cellIndex < currentRow.length - 1;
 
-      if (rightCheck) {
-        nearCells.push(currentRow[cellIndex + 1]);
-        if (prevRow) {
-          nearCells.push(prevRow[cellIndex + 1])
-        }
-        if (nextRow) {
-          nearCells.push(nextRow[cellIndex + 1])
-        }
+    if (rightCheck) {
+      nearCells.push(currentRow[cellIndex + 1]);
+      if (prevRow) {
+        nearCells.push(prevRow[cellIndex + 1])
       }
-      if (leftCheck) {
-        nearCells.push(currentRow[cellIndex - 1]);
-        if (prevRow) {
-          nearCells.push(prevRow[cellIndex - 1])
-        }
-        if (nextRow) {
-          nearCells.push(nextRow[cellIndex - 1])
-        }
+      if (nextRow) {
+        nearCells.push(nextRow[cellIndex + 1])
       }
-      console.log(nearCells);
-      return nearCells;
-    };
-
-
-
+    }
+    if (leftCheck) {
+      nearCells.push(currentRow[cellIndex - 1]);
+      if (prevRow) {
+        nearCells.push(prevRow[cellIndex - 1])
+      }
+      if (nextRow) {
+        nearCells.push(nextRow[cellIndex - 1])
+      }
+    }
+    return nearCells;
+  };
+  
 
 
 });
